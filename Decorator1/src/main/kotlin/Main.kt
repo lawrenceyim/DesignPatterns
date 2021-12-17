25d3388cf33f1d2cf7bb9a1ccdddc2e2eb6068c4
Lawrence Y <84044148+lawrenceyim@users.noreply.github.com>
2021-12-17 13:56:01 -0500

    updated code for Decorator1

--- Decorator1/src/main/kotlin/Main.kt
+++ Decorator1/src/main/kotlin/Main.kt
@@ -2,7 +2,9 @@ fun main() {
     val coffee = Mocha(Espresso())
     val beverage = coffee.decorate()
     println(beverage)
+    println(coffee.cost())
 
     val coffee1 = Marshmallows(Mocha(HouseBlend()))
     println(coffee1.decorate())
+    println(coffee1.cost())
 }
